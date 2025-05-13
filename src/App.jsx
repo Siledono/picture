d0daf0ed90b2614de5c462d6256b2242e60ef54b
Siledono <siledono100@gmail.com>
2025-05-12 17:54:27 -0700

    Implementando API de unsplash

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
       <h1>Pictures App</h1>
-      <searchBar onSumbit={handleSumbit}/>
+      <SearchBar onSumbit={handleSumbit}/>
       <ImageList images={images}/> 
     </>
   )
